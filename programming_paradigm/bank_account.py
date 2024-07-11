4869f53aa709af844995d5b72ded143b51175d04
Mouhssin Aissam <koala3901@gmail.com>
2024-07-11 18:05:43 +0100

    pleaseee

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,22 +1,23 @@
 class BankAccount:
-    def __init__(self,account_balance=0):
+    def __init__(self, account_balance=0):
         self.balance = account_balance
-        
-    def deposit(self,amount):
-        if amount>0:
+
+    def deposit(self, amount):
+        if amount > 0:
             self.balance += amount
-            return f"Deposited: ${amount}"
+            return f"${amount} deposited. New balance: ${self.balance}"
         else:
-            return "Enter a positive amount"
-    def withdraw (self,amount):
+            return "Enter a positive amount."
+
+    def withdraw(self, amount):
         if amount > 0 and self.balance >= amount:
             self.balance -= amount
             return True
         elif amount <= 0:
-            return "Enter a positive amount"
+            return "Enter a positive amount."
         else:
             return False
-        pass
+
     def display_balance(self):
-        return f"Current balance: ${self.balance}"
+        print(f"Current Balance: ${self.balance:.2f}")
        
